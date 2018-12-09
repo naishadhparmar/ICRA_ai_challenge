2260cad12e9a26f71f5ca7fd44061f42a03d1500
Naishadh Parmar <naishadh_parmar@live.com>
2018-12-09 16:27:50 +0530

    Add static planning using move_base

--- mapper/src/laserscan_to_pointcloud_converter.cpp
+++ mapper/src/laserscan_to_pointcloud_converter.cpp
@@ -3,7 +3,7 @@
 
 ros::Publisher pointcloud_pub;
 
-void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in) {
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
     laser_geometry::LaserProjection projector_;
     sensor_msgs::PointCloud2 cloud;
     projector_.projectLaser(*scan_in, cloud);
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
     pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/cloud_in", 1000);
     ros::NodeHandle nh_prv("~");
-    std::string laserscan_topic = "/scan";
+    std::string laserscan_topic = "/p3dx/laser/scan";
     nh_prv.getParam("laserscan", laserscan_topic);
     ros::Subscriber laserscan_sub = nh.subscribe(laserscan_topic, 1000, scanCallback);
     ros::spin();
